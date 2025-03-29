16f3d21547da7e38d81e088800baeeeee8907ab5
RaulRMO <raulmartinsagrivet1@gmail.com>
2025-03-29 01:18:04 -0300

    add
    Formulário de cadastro de clientes

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,6 @@
-<link rel="stylesheet" href="/consultorio-dentista/public/assets/css/style.css">
+<nav>
+    <a href="/consultorio-dentista/">Home</a>
+    <a href="/consultorio-dentista/clientes.php">Clientes</a>
+    <a href="/consultorio-dentista/novo_cliente.php">Novo Cliente</a>
+    <a href="/consultorio-dentista/consultas.php">Consultas</a>
+</nav>
